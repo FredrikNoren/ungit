fd8c65d770d6846d2295190e2fa5d1a6f771ad79
Fredrik Noren <fredrik.noren@keldyn.com>
2013-05-24 22:07:45 +0200

    Move out Vector2

--- public/scripts/logrenderer.js
+++ public/scripts/logrenderer.js
@@ -2,35 +2,6 @@
 
 var logRenderer = {};
 
-var Vector2 = function(x, y) {
-	this.x = x;
-	this.y = y;
-}
-Vector2.prototype.length = function() {
-	return Math.sqrt(this.x * this.x + this.y * this.y);
-}
-Vector2.prototype.normalized = function() {
-	var length = this.length();
-	return new Vector2(this.x / length, this.y / length);
-}
-Vector2.prototype.sub = function(v) {
-	return Vector2.sub(this, v);
-}
-Vector2.prototype.add = function(v) {
-	return Vector2.add(this, v);
-}
-Vector2.prototype.mul = function(v) {
-	return Vector2.mul(this, v);
-}
-Vector2.sub = function(a, b) {
-	return new Vector2(a.x - b.x, a.y - b.y);
-}
-Vector2.add = function(a, b) {
-	return new Vector2(a.x + b.x, a.y + b.y);
-}
-Vector2.mul = function(a, val) {
-	return new Vector2(a.x * val, a.y * val);
-}
 
 var LogNodeIcon = function(logEntry) {
 	this.logEntry = logEntry;
@@ -103,6 +74,7 @@ var markBranches = function(log, logGraph) {
 	});
 	recursivelyMarkBranch(HEAD, HEAD.branch);
 }
+
 
 var randomColor = function() {
 	var randomHex = function() {
---
+++ public/test/spec.vector2.js
@@ -0,0 +1,11 @@
+
+
+describe('Vector2', function() {
+	it('should be addable', function() {
+		var a = new Vector2(5, 2);
+		var b = new Vector2(8, 10);
+		var c = a.add(b);
+		expect(c.x).to.be(a.x + b.x);
+		expect(c.y).to.be(a.y + b.y);
+	})
+})
