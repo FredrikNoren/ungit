145d83e493aef6b68f071d52c068b6c5661d796f
Fredrik Noren <fredrik.noren@keldyn.com>
2013-05-29 00:09:32 +0200

    Rework branch utils box

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -120,12 +120,6 @@ RepositoryViewModel.prototype.fetch = function() {
 		self.isFetching(false);
 	});
 }
-RepositoryViewModel.prototype.pull = function() {
-	api.query('POST', '/rebase', { path: this.path, onto: 'origin/' + this.graph.activeBranch() });
-}
-RepositoryViewModel.prototype.push = function() {
-	api.query('POST', '/push', { path: this.path });
-}
 RepositoryViewModel.prototype.updateStatus = function(opt_callback) {
 	var self = this;
 	api.query('GET', '/status', { path: this.path }, function(err, status){
