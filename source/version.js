71df72ec5f2ce52751d94235a2a2db4b57f2dc03
Fredrik Noren <fredrik.noren@keldyn.com>
2013-08-15 23:49:52 +0200

    Better dev version name

--- source/version.js
+++ source/version.js
@@ -4,24 +4,33 @@ var npm = require('npm');
 
 var cachedVersion;
 
-exports.getVersion = function(callback) {
+var version = exports;
+
+version.getVersion = function(callback) {
 	if (cachedVersion) callback(cachedVersion);
-	if (fs.existsSync(__dirname + '/../.git')) {
-		child_process.exec('git rev-parse --short HEAD', { cwd: __dirname + '/../' }, function(err, revision) {
-			revision.replace('\n', ' ');
-			revision = revision.trim();
-			cachedVersion = 'dev-' + revision;
+	version.getPackageJsonVersion(function(packageJsonVersion) {
+		if (fs.existsSync(__dirname + '/../.git')) {
+			child_process.exec('git rev-parse --short HEAD', { cwd: __dirname + '/../' }, function(err, revision) {
+				revision.replace('\n', ' ');
+				revision = revision.trim();
+
+				cachedVersion = 'dev-' + packageJsonVersion + '-' + revision;
+				callback(cachedVersion);
+			});
+		} else {
+			cachedVersion = packageJsonVersion;
 			callback(cachedVersion);
-		});
-	} else {
-		var packageJson = fs.readFileSync(__dirname + '/../package.json');
-		packageJson = JSON.parse(packageJson);
-		cachedVersion = packageJson.version;
-		callback(cachedVersion);
-	}
+		}
+	});
 }
 
-exports.getLatestVersion = function(callback) {
+version.getPackageJsonVersion = function(callback) {
+	var packageJson = fs.readFileSync(__dirname + '/../package.json');
+	packageJson = JSON.parse(packageJson);
+	callback(packageJson.version);
+}
+
+version.getLatestVersion = function(callback) {
 	var packageName = 'ungit';
 	npm.load(function() {
 		npm.commands.show([packageName, 'versions'], true, function(err, data) {
