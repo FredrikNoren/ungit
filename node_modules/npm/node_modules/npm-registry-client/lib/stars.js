48eeaeadd2da23ea8736d2e65f9d40b146595d13
Sean Lang <slang800@gmail.com>
2013-08-21 04:22:56 -0500

    remove node_modules ... this is all installed by npm and shouldn't be in the repo

--- node_modules/npm/node_modules/npm-registry-client/lib/stars.js
+++
@@ -1,9 +0,0 @@
-var qs = require('querystring')
-
-module.exports = stars
-
-function stars (name, cb) {
-  name = encodeURIComponent(name)
-  var path = "/-/_view/starredByUser?key=\""+name+"\""
-  this.request("GET", path, cb)
-}
--- node_modules/npm/node_modules/npm-registry-client/lib/tag.js
+++
@@ -1,6 +0,0 @@
-
-module.exports = tag
-
-function tag (project, version, tag, cb) {
-  this.request("PUT", project+"/"+tag, JSON.stringify(version), cb)
-}
