911f82dea93594cd7304a03146c34918aa2dfd2f
Fredrik Norén <noren@spotify.com>
2013-11-01 20:15:17 +0100

    Standardize clicktest tags
    Change-Id: I4e3d3a05ddce3f544b8f84718d6518816116497c

--- clicktests/clicktests.js
+++ clicktests/clicktests.js
@@ -43,7 +43,7 @@ test('Init', function(done) {
 
 test('Open home screen', function(done) {
 	page.open('http://localhost:' + config.port, function() {
-		helpers.waitForElement(page, '[data-ta="home-page"]', function() {
+		helpers.waitForElement(page, '[data-ta-container="home-page"]', function() {
 			done();
 		});
 	});
@@ -72,40 +72,40 @@ test('Create test directory', function(done) {
 
 test('Open path screen', function(done) {
 	page.open('http://localhost:' + config.port + '/#/repository?path=' + encodeURIComponent(testRepoPath), function () {
-		helpers.waitForElement(page, '[data-ta="uninited-path-page"]', function() {
+		helpers.waitForElement(page, '[data-ta-container="uninited-path-page"]', function() {
 			done();
 		});
 	});
 });
 
 test('Init repository should bring you to repo page', function(done) {
-	helpers.click(page, '[data-ta="init-repository"]');
-	helpers.waitForElement(page, '[data-ta="repository-view"]', function() {
-		helpers.expectNotFindElement(page, '[data-ta="remote-error-popup"]');
+	helpers.click(page, '[data-ta-clickable="init-repository"]');
+	helpers.waitForElement(page, '[data-ta-container="repository-view"]', function() {
+		helpers.expectNotFindElement(page, '[data-ta-container="remote-error-popup"]');
 		done();
 	});
 });
 
 test('Clicking logo should bring you to home screen', function(done) {
-	helpers.click(page, '[data-ta="home-link"]');
-	helpers.waitForElement(page, '[data-ta="home-page"]', function() {
+	helpers.click(page, '[data-ta-clickable="home-link"]');
+	helpers.waitForElement(page, '[data-ta-container="home-page"]', function() {
 		done();
 	});
 });
 
 test('Entering an invalid path should bring you to an error screen', function(done) {
-	helpers.click(page, '[data-ta="navigation-path"]');
+	helpers.click(page, '[data-ta-input="navigation-path"]');
 	helpers.write(page, '/a/path/that/doesnt/exist\n');
-	helpers.waitForElement(page, '[data-ta="invalid-path"]', function() {
+	helpers.waitForElement(page, '[data-ta-container="invalid-path"]', function() {
 		done();
 	});
 });
 
 var enterRepoByTypingPath = function(path, callback) {
-	helpers.click(page, '[data-ta="navigation-path"]');
+	helpers.click(page, '[data-ta-input="navigation-path"]');
 	helpers.selectAllText(page);
 	helpers.write(page, path + '\n');
-	helpers.waitForElement(page, '[data-ta="repository-view"]', function() {
+	helpers.waitForElement(page, '[data-ta-container="repository-view"]', function() {
 		callback();
 	});
 }
@@ -117,12 +117,12 @@ test('Entering a path to a repo should bring you to that repo', function(done) {
 var createCommitWithNewFile = function(fileName, commitMessage, callback) {
 	createTestFile(testRepoPath + '/' + fileName, function(err) {
 		if (err) return callback(err);
-		helpers.waitForElement(page, '[data-ta="staging-file"]', function() {
-			helpers.click(page, '[data-ta="staging-commit-title"]')
+		helpers.waitForElement(page, '[data-ta-container="staging-file"]', function() {
+			helpers.click(page, '[data-ta-input="staging-commit-title"]')
 			helpers.write(page, commitMessage);
 			setTimeout(function() {
-				helpers.click(page, '[data-ta="commit"]');
-				helpers.waitForNotElement(page, '[data-ta="staging-file"]', function() {
+				helpers.click(page, '[data-ta-clickable="commit"]');
+				helpers.waitForNotElement(page, '[data-ta-container="staging-file"]', function() {
 					setTimeout(function() { // let the animation finish
 						callback();
 					}, 1000);
@@ -134,7 +134,7 @@ var createCommitWithNewFile = function(fileName, commitMessage, callback) {
 
 test('Should be possible to create and commit a file', function(done) {
 	createCommitWithNewFile('testfile.txt', 'Init', function() {
-		helpers.waitForElement(page, '[data-ta="node"]', function() {
+		helpers.waitForElement(page, '[data-ta-container="node"]', function() {
 			done();
 		});
 	})
@@ -143,14 +143,14 @@ test('Should be possible to create and commit a file', function(done) {
 test('Should be able to add a new file to .gitignore', function(done) {
 	createTestFile(testRepoPath + '/addMeToIgnore.txt', function(err) {
 		if (err) return done(err);
-                helpers.waitForElement(page, '[data-ta="staging-file"]', function() {
+                helpers.waitForElement(page, '[data-ta-container="staging-file"]', function() {
 			// add "addMeToIgnore.txt" to .gitignore
-			helpers.click(page, '[data-ta="ignore-file"]');
+			helpers.click(page, '[data-ta-clickable="ignore-file"]');
 			// add ".gitignore" to .gitignore
 			//TODO I'm not sure what is the best way to detect page refresh, so currently wait for 1 sec and then click ignore-file. 
 			setTimeout(function() {
-                        	helpers.click(page, '[data-ta="ignore-file"]');
-                	        helpers.waitForNotElement(page, '[data-ta="staging-file"]', function() {
+                        	helpers.click(page, '[data-ta-clickable="ignore-file"]');
+                	        helpers.waitForNotElement(page, '[data-ta-container="staging-file"]', function() {
         	                        done();
 	                        });
 			}, 1000);
@@ -161,9 +161,9 @@ test('Should be able to add a new file to .gitignore', function(done) {
 test('Should be possible to discard a created file', function(done) {
 	createTestFile(testRepoPath + '/testfile2.txt', function(err) {
 		if (err) return done(err);
-		helpers.waitForElement(page, '[data-ta="staging-file"]', function() {
-			helpers.click(page, '[data-ta="discard-file"]');
-			helpers.waitForNotElement(page, '[data-ta="staging-file"]', function() {
+		helpers.waitForElement(page, '[data-ta-container="staging-file"]', function() {
+			helpers.click(page, '[data-ta-clickable="discard-file"]');
+			helpers.waitForNotElement(page, '[data-ta-container="staging-file"]', function() {
 				done();
 			});
 		});
@@ -172,12 +172,12 @@ test('Should be possible to discard a created file', function(done) {
 
 var createRef = function(name, type, callback) {
 	helpers.log('Createing branch ' + name);
-	helpers.click(page, '[data-ta="show-new-branch-form"]');
-	helpers.click(page, '[data-ta="new-branch-name"]');
+	helpers.click(page, '[data-ta-clickable="show-new-branch-form"]');
+	helpers.click(page, '[data-ta-input="new-branch-name"]');
 	helpers.write(page, name);
 	setTimeout(function() {
-		helpers.click(page, '[data-ta="create-' + type + '"]');
-		helpers.waitForElement(page, '[data-ta="' + type + '"][data-ta-name="' + name + '"]', function() {
+		helpers.click(page, '[data-ta-clickable="create-' + type + '"]');
+		helpers.waitForElement(page, '[data-ta-clickable="' + type + '"][data-ta-name="' + name + '"]', function() {
 			callback();
 		});
 	}, 100);
@@ -196,9 +196,9 @@ test('Should be possible to create a branch', function(done) {
 
 test('Should be possible to create and destroy a branch', function(done) {
 	createBranch('willbedeleted', function() {
-		helpers.click(page, '[data-ta="branch"][data-ta-name="willbedeleted"]');
+		helpers.click(page, '[data-ta-clickable="branch"][data-ta-name="willbedeleted"]');
 		helpers.click(page, '[data-ta-action="delete"][data-ta-visible="true"]');
-		helpers.waitForNotElement(page, '[data-ta="branch"][data-ta-name="willbedeleted"]', function() {
+		helpers.waitForNotElement(page, '[data-ta-clickable="branch"][data-ta-name="willbedeleted"]', function() {
 			done();
 		});
 	});
@@ -206,9 +206,9 @@ test('Should be possible to create and destroy a branch', function(done) {
 
 test('Should be possible to create and destroy a tag', function(done) {
 	createTag('tagwillbedeleted', function() {
-		helpers.click(page, '[data-ta="tag"][data-ta-name="tagwillbedeleted"]');
+		helpers.click(page, '[data-ta-clickable="tag"][data-ta-name="tagwillbedeleted"]');
 		helpers.click(page, '[data-ta-action="delete"][data-ta-visible="true"]');
-		helpers.waitForNotElement(page, '[data-ta="tag"][data-ta-name="tagwillbedeleted"]', function() {
+		helpers.waitForNotElement(page, '[data-ta-clickable="tag"][data-ta-name="tagwillbedeleted"]', function() {
 			done();
 		});
 	});
@@ -217,12 +217,12 @@ test('Should be possible to create and destroy a tag', function(done) {
 test('Commit changes to a file', function(done) {
 	changeTestFile(testRepoPath + '/testfile.txt', function(err) {
 		if (err) return done(err);
-		helpers.waitForElement(page, '[data-ta="staging-file"]', function() {
-			helpers.click(page, '[data-ta="staging-commit-title"]')
+		helpers.waitForElement(page, '[data-ta-container="staging-file"]', function() {
+			helpers.click(page, '[data-ta-input="staging-commit-title"]')
 			helpers.write(page, 'My commit message');
 			setTimeout(function() {
-				helpers.click(page, '[data-ta="commit"]');
-				helpers.waitForNotElement(page, '[data-ta="staging-file"]', function() {
+				helpers.click(page, '[data-ta-clickable="commit"]');
+				helpers.waitForNotElement(page, '[data-ta-container="staging-file"]', function() {
 					done();
 				});
 			}, 100);
@@ -231,9 +231,9 @@ test('Commit changes to a file', function(done) {
 });
 
 function checkout(page, branch, callback) {
-	helpers.click(page, '[data-ta="branch"][data-ta-name="' + branch + '"]');
+	helpers.click(page, '[data-ta-clickable="branch"][data-ta-name="' + branch + '"]');
 	helpers.click(page, '[data-ta-action="checkout"][data-ta-visible="true"]');
-	helpers.waitForElement(page, '[data-ta="branch"][data-ta-name="' + branch + '"][data-ta-current="true"]', function() {
+	helpers.waitForElement(page, '[data-ta-clickable="branch"][data-ta-name="' + branch + '"][data-ta-current="true"]', function() {
 		callback();
 	});
 }
@@ -247,7 +247,7 @@ test('Create another commit', function(done) {
 });
 
 function refAction(page, ref, local, action, callback) {
-	helpers.click(page, '[data-ta="branch"][data-ta-name="' + ref + '"][data-ta-local="' + local + '"]');
+	helpers.click(page, '[data-ta-clickable="branch"][data-ta-name="' + ref + '"][data-ta-local="' + local + '"]');
 	helpers.mousemove(page, '[data-ta-action="' + action + '"][data-ta-visible="true"]');
 	setTimeout(function() { // Wait for next animation frame
 		helpers.click(page, '[data-ta-action="' + action + '"][data-ta-visible="true"]');
@@ -276,7 +276,7 @@ test('Merge', function(done) {
 });
 
 function moveRef(page, ref, targetNodeCommitTitle, callback) {
-	helpers.click(page, '[data-ta="branch"][data-ta-name="' + ref + '"]');
+	helpers.click(page, '[data-ta-clickable="branch"][data-ta-name="' + ref + '"]');
 	helpers.mousemove(page, '[data-ta-node-title="' + targetNodeCommitTitle + '"] [data-ta-action="move"][data-ta-visible="true"]');
 	setTimeout(function() { // Wait for next animation frame
 		helpers.click(page, '[data-ta-node-title="' + targetNodeCommitTitle + '"] [data-ta-action="move"][data-ta-visible="true"]');
@@ -306,15 +306,15 @@ test('Create a bare repo (not in ui)', function(done) {
 });
 
 test('Adding a remote', function(done) {
-	helpers.click(page, '[data-ta="remotes-menu"]');
-	helpers.click(page, '[data-ta="show-add-remote-dialog"]');
-	helpers.waitForElement(page, '[data-ta-dialog="add-remote"]', function() {
-		helpers.click(page, '[data-ta-dialog="add-remote"] [data-ta="name"]');
+	helpers.click(page, '[data-ta-clickable="remotes-menu"]');
+	helpers.click(page, '[data-ta-clickable="show-add-remote-dialog"]');
+	helpers.waitForElement(page, '[data-ta-container="add-remote"]', function() {
+		helpers.click(page, '[data-ta-container="add-remote"] [data-ta-input="name"]');
 		helpers.write(page, 'myremote');
-		helpers.click(page, '[data-ta-dialog="add-remote"] [data-ta="url"]');
+		helpers.click(page, '[data-ta-container="add-remote"] [data-ta-input="url"]');
 		helpers.write(page, bareRepoPath);
-		helpers.click(page, '[data-ta-dialog="add-remote"] [data-ta="submit"]');
-		helpers.waitForElement(page, '[data-ta-remote="myremote"]', function() {
+		helpers.click(page, '[data-ta-container="add-remote"] [data-ta-clickable="submit"]');
+		helpers.waitForElement(page, '[data-ta-container="remotes"] [data-ta-clickable="myremote"]', function() {
 			done();
 		});
 	});
@@ -331,23 +331,23 @@ test('Fetch from newly added remote', function(done) {
 var testClonePath;
 
 test('Enter path to test root', function(done) {
-	helpers.click(page, '[data-ta="navigation-path"]');
+	helpers.click(page, '[data-ta-input="navigation-path"]');
 	helpers.selectAllText(page);
 	helpers.write(page, testRootPath + '\n');
-	helpers.waitForElement(page, '[data-ta="uninited-path-page"]', function() {
+	helpers.waitForElement(page, '[data-ta-container="uninited-path-page"]', function() {
 		done();
 	});
 });
 
 test('Clone repository should bring you to repo page', function(done) {
 	testClonePath = testRootPath + '/testclone';
-	helpers.click(page, '[data-ta="clone-url-input"]');
+	helpers.click(page, '[data-ta-input="clone-url"]');
 	helpers.write(page, testRepoPath);
-	helpers.click(page, '[data-ta="clone-target-input"]');
+	helpers.click(page, '[data-ta-input="clone-target"]');
 	helpers.write(page, testClonePath);
-	helpers.click(page, '[data-ta="clone-repository"]');
-	helpers.waitForElement(page, '[data-ta="repository-view"]', function() {
-		helpers.expectNotFindElement(page, '[data-ta="remote-error-popup"]');
+	helpers.click(page, '[data-ta-clickable="clone-repository"]');
+	helpers.waitForElement(page, '[data-ta-container="repository-view"]', function() {
+		helpers.expectNotFindElement(page, '[data-ta-container="remote-error-popup"]');
 		setTimeout(function() { // Let animations finish
 			done();
 		}, 1000);
@@ -355,9 +355,9 @@ test('Clone repository should bring you to repo page', function(done) {
 });
 
 var fetch = function(callback) {
-	helpers.click(page, '[data-ta="fetch"]');
-	helpers.waitForElement(page, '[data-ta="fetch"] [data-ta="progress-bar"]', function() {
-		helpers.waitForNotElement(page, '[data-ta="fetch"] [data-ta="progress-bar"]', function() {
+	helpers.click(page, '[data-ta-clickable="fetch"]');
+	helpers.waitForElement(page, '[data-ta-clickable="fetch"] [data-ta-element="progress-bar"]', function() {
+		helpers.waitForNotElement(page, '[data-ta-clickable="fetch"] [data-ta-element="progress-bar"]', function() {
 			callback();
 		});
 	});
@@ -375,12 +375,12 @@ test('Should be possible to create and push a branch', function(done) {
 
 test('Should be possible to force push a branch', function(done) {
 	moveRef(page, 'branchinclone', 'Init', function() {
-		helpers.click(page, '[data-ta="branch"][data-ta-name="branchinclone"][data-ta-local="true"]');
+		helpers.click(page, '[data-ta-clickable="branch"][data-ta-name="branchinclone"][data-ta-local="true"]');
 		helpers.mousemove(page, '[data-ta-action="push"][data-ta-visible="true"]');
 		setTimeout(function() { // Wait for next animation frame
 			helpers.click(page, '[data-ta-action="push"][data-ta-visible="true"]');
-			helpers.waitForElement(page, '[data-ta="yes-no-dialog"]', function() {
-				helpers.click(page, '[data-ta="yes"]');
+			helpers.waitForElement(page, '[data-ta-container="yes-no-dialog"]', function() {
+				helpers.click(page, '[data-ta-clickable="yes"]');
 				helpers.waitForNotElement(page, '[data-ta-action="push"][data-ta-visible="true"]', function() {
 					setTimeout(function() {
 						done();
@@ -395,15 +395,15 @@ test('Should be possible to force push a branch', function(done) {
 // Shutdown
 
 test('Go to home screen', function(done) {
-	helpers.click(page, '[data-ta="home-link"]');
-	helpers.waitForElement(page, '[data-ta="home-page"]', function() {
+	helpers.click(page, '[data-ta-clickable="home-link"]');
+	helpers.waitForElement(page, '[data-ta-container="home-page"]', function() {
 		done();
 	});
 });
 
 test('Shutdown server should bring you to connection lost page', function(done) {
 	shutdownServer(function() {
-		helpers.waitForElement(page, '[data-ta="user-error-page"]', function() {
+		helpers.waitForElement(page, '[data-ta-container="user-error-page"]', function() {
 			done();
 		});
 	});
@@ -419,33 +419,33 @@ test('Start with authentication', function(done) {
 
 test('Open home screen should show authentication dialog', function(done) {
 	page.open('http://localhost:' + config.port, function() {
-		helpers.waitForElement(page, '[data-ta="login-page"]', function() {
+		helpers.waitForElement(page, '[data-ta-container="login-page"]', function() {
 			done();
 		});
 	});
 });
 
 test('Filling out the authentication with wrong details should result in an error', function(done) {
-	helpers.click(page, '[data-ta="login-page"] [data-ta="input-username"]');
+	helpers.click(page, '[data-ta-container="login-page"] [data-ta-input="username"]');
 	helpers.write(page, testuser.username);
-	helpers.click(page, '[data-ta="login-page"] [data-ta="input-password"]');
+	helpers.click(page, '[data-ta-container="login-page"] [data-ta-input="password"]');
 	helpers.write(page, 'notthepassword');
-	helpers.click(page, '[data-ta="login-page"] [data-ta="submit"]');
-	helpers.waitForElement(page, '[data-ta="login-error"]', function() {
-		helpers.expectNotFindElement(page, '[data-ta="home-page"]')
+	helpers.click(page, '[data-ta-container="login-page"] [data-ta-clickable="submit"]');
+	helpers.waitForElement(page, '[data-ta-element="login-error"]', function() {
+		helpers.expectNotFindElement(page, '[data-ta-container="home-page"]')
 		done();
 	});
 });
 
 test('Filling out the authentication should bring you to the home screen', function(done) {
-	helpers.click(page, '[data-ta="login-page"] [data-ta="input-username"]');
+	helpers.click(page, '[data-ta-container="login-page"] [data-ta-input="username"]');
 	helpers.selectAllText(page);
 	helpers.write(page, testuser.username);
-	helpers.click(page, '[data-ta="login-page"] [data-ta="input-password"]');
+	helpers.click(page, '[data-ta-container="login-page"] [data-ta-input="password"]');
 	helpers.selectAllText(page);
 	helpers.write(page, testuser.password);
-	helpers.click(page, '[data-ta="login-page"] [data-ta="submit"]');
-	helpers.waitForElement(page, '[data-ta="home-page"]', function() {
+	helpers.click(page, '[data-ta-container="login-page"] [data-ta-clickable="submit"]');
+	helpers.waitForElement(page, '[data-ta-container="home-page"]', function() {
 		done();
 	});
 });
@@ -453,7 +453,7 @@ test('Filling out the authentication should bring you to the home screen', funct
 test('Cleanup and shutdown server', function(done) {
 	backgroundAction('POST', 'http://localhost:' + config.port + '/api/testing/cleanup', function() {
 		shutdownServer(function() {
-			helpers.waitForElement(page, '[data-ta="user-error-page"]', function() {
+			helpers.waitForElement(page, '[data-ta-container="user-error-page"]', function() {
 				done();
 			});
 		});
