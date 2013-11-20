2c173212cb7ade2a03c407448dbef1c57a6dc8a8
Fredrik Noren <fredrik.noren@keldyn.com>
2013-11-20 21:48:22 +0100

    Make sure git results are strings

--- source/git.js
+++ source/git.js
@@ -85,6 +85,8 @@ var gitQueue = async.queue(function (task, callback) {
   if (config.logGitCommands) winston.info('git executing: ' + task.repoPath + ' ' + task.command + ' ' + task.encoding);
   var process = child_process.exec(task.command, { cwd: task.repoPath, maxBuffer: 1024 * 1024 * 10, encoding: task.encoding},
     function (error, stdout, stderr) {
+      stdout = stdout.toString(); // Convert Buffers to strings
+      stderr = stderr.toString();
       if (config.logGitOutput) winston.info('git result (first 400 bytes): ' + task.command + '\n' + stderr.slice(0, 400) + '\n' + stdout.slice(0, 400));
       if (error !== null) {
         var err = {};
