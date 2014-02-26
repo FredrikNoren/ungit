d8241fd282b85c8648ca9a722f4352c33d6edfa3
Fredrik Noren <noren@spotify.com>
2014-02-25 20:08:23 -0500

    Fix cache bug

--- source/utils/cache.js
+++ source/utils/cache.js
@@ -12,7 +12,7 @@ var cache = function(constructValue) {
     if (hasCache) return callback(f.error, f.value);
     if (constructDone) return constructDone.add(callback);
 
-    constructDone = new signals.Signal();
+    var localConstructDone = constructDone = new signals.Signal();
     constructDone.add(function(err, val) {
       constructDone = null;
       callback(err, val);
@@ -21,7 +21,7 @@ var cache = function(constructValue) {
       hasCache = true;
       f.error = err;
       f.value = value;
-      constructDone.dispatch(err, value);
+      localConstructDone.dispatch(err, value);
     });
   };
 
