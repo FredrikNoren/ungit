3e3726e71554b71b3cdd376d699353519f2bf111
thomas <thomas@asia.cybridge.jp>
2014-03-27 05:36:04 -0400

    xoa

--- public/source/program-events.js
+++
@@ -1,9 +0,0 @@
-
-var signals = require('signals');
-
-var programEvents = new signals.Signal();
-module.exports = programEvents;
-
-programEvents.add(function(event) {
-  console.log('Event:', event);
-});
