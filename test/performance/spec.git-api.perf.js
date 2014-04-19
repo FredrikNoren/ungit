967b01f4d6b4803265a266f88ebdf24c37354257
CodingTwinky <twinky@codingtwinky.com>
2014-04-19 14:40:47 -0500

    Smaller test data intervals

--- test/performance/spec.git-api.perf.js
+++ test/performance/spec.git-api.perf.js
@@ -32,14 +32,14 @@ describe('performance testing', function () {
     });
   });
 
-  var oneMbFile = '1mbFile.txt';
-  var tenMbFile = '10mbFile.txt';
-  var thirtyMbFile = '30mbFile.txt';
+  var oneMbFile = 'smallFile.txt';
+  var fiveMbFile = 'mediumFile.txt';
+  var tenMbFile = 'largeFile.txt';
 
   var generateTestFiles = function() {
     testFileGenerator.generateTestFile(path.join(testDir, oneMbFile), 1024);
+    testFileGenerator.generateTestFile(path.join(testDir, fiveMbFile), 5120);
     testFileGenerator.generateTestFile(path.join(testDir, tenMbFile), 10240);
-    testFileGenerator.generateTestFile(path.join(testDir, thirtyMbFile), 30720);
   }
 
   // Need a more realistic way to alter file as this doesn't have 100% test coverage
@@ -49,7 +49,7 @@ describe('performance testing', function () {
 
   it('create test files', function(done) {
     generateTestFiles();
-    common.post(req, '/commit', { path: testDir, message: 'whateve', files: [oneMbFile, tenMbFile, thirtyMbFile] }, function() {
+    common.post(req, '/commit', { path: testDir, message: 'whateve', files: [oneMbFile, fiveMbFile, tenMbFile] }, function() {
       changeTestFiles();
       done();
     });
@@ -57,5 +57,17 @@ describe('performance testing', function () {
 
   it('git log performance test', function(done) {
     performanceHelper.runner(req, '/log', { path: testDir }, 10, done);
+  });
+
+  it('git diff performance test on 1mb File', function(done) {
+    performanceHelper.runner(req, '/diff', { path: testDir, file: oneMbFile }, 10, done);
+  });
+
+  it('git diff performance test on 5mb File', function(done) {
+    performanceHelper.runner(req, '/diff', { path: testDir, file: fiveMbFile }, 10, done);
+  });
+
+  it('git diff performance test on 10mb File', function(done) {
+    performanceHelper.runner(req, '/diff', { path: testDir, file: tenMbFile }, 10, done);
   });
 });
