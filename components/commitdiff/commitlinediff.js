074682de4c746e19ab8b083074bc7ca093720c4f
CodingTwinky <twinky@codingtwinky.com>
2014-04-19 23:52:29 -0500

    Added a check prompt for big diffs

--- components/commitdiff/commitlinediff.js
+++ components/commitdiff/commitlinediff.js
@@ -1,6 +1,7 @@
 var ko = require('knockout');
 var components = require('ungit-components');
 var inherits = require('util').inherits;
+var programEvents = require('ungit-program-events');
 
 var imageFileExtensions = ['PNG', 'JPG', 'BMP', 'GIF', 'JPEG'];
 
@@ -19,20 +20,41 @@ var CommitLineDiff = function(args) {
 exports.CommitLineDiff = CommitLineDiff;
 
 CommitLineDiff.prototype.fileNameClick = function(data, event) {
+  var self = this;
+
   if (this.showSpecificDiff()) {
     this.showSpecificDiff(false);
   } else {
-    var self = this;
-    if (this.type() == 'textdiff' && !this.specificDiff().diffs()) {
-      this.specificDiff().invalidateDiff(function() {
-        self.showSpecificDiff(true);
-      });      
+    if (this.added() + this.removed() < 100 || this.isLoaded()) {
+      this.showDiff();
     } else {
-      self.showSpecificDiff(true);
+      var checkPrompt = components.create('yesnodialog', { 'title': 'Are you sure?', 'details': 'Diff is too big, are you sure you want to see the diff?'});
+      checkPrompt.closed.add(function() {
+        if (checkPrompt.result()) {
+          self.showDiff();
+        }
+      });
+      programEvents.dispatch({ event: 'request-show-dialog', dialog: checkPrompt });
     }
   }
+  
   event.stopImmediatePropagation();
 };
+
+CommitLineDiff.prototype.showDiff = function() {
+  var self = this;
+  if (!this.isLoaded()) {
+    this.specificDiff().invalidateDiff(function() {
+      self.showSpecificDiff(true);
+    });      
+  } else {
+    self.showSpecificDiff(true);
+  }
+}
+
+CommitLineDiff.prototype.isLoaded = function() {
+  return this.type() === 'textdiff' && this.specificDiff().diffs();
+}
 
 CommitLineDiff.prototype.type = function() {
   if (!this.fileName()) {
