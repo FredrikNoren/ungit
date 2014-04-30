e323d724fb7023ed6c825917c0dacbea5f657545
CodingTwinky <twinky@codingtwinky.com>
2014-04-29 21:25:35 -0500

    Unittest fix and adding variable initial lines display limit test

--- test/spec.git-parser.js
+++ test/spec.git-parser.js
@@ -20,24 +20,38 @@ describe('git-parser stash show', function () {
 });
 
 describe('git-parse diff on big change', function() {
-  it('diff on big change sould show first 50 lines of changes first', function() {
+  it('diff on big change sould show display limit amount of changed lines first', function() {
     var sampleText = "diff --git a/source/git-parser.js b/source/git-parser.js\nindex 643e964..df31474 100644\n--- a/source/git-parser.js\n+++ b/source/git-parser.js\n@@ -22,12 +22,12 @@ exports.parseGitStatus = function(text) {\n   return result;\n };\n";
     sampleText += '1\n2\n3\n4\n5\n6\n7\n8\n9\n0\n1\n2\n3\n4\n5\n6\n7\n8\n9\n0\n1\n2\n3\n4\n5\n6\n7\n8\n9\n0\n';
     sampleText += '1\n2\n3\n4\n5\n6\n7\n8\n9\n0\n1\n2\n3\n4\n5\n6\n7\n8\n9\n0\n1\n2\n3\n4\n5\n6\n7\n8\n9\n0\n';
 
-    var res = gitParser.parseGitDiff(sampleText, ['false']);
+    var res = gitParser.parseGitDiff(sampleText, {isLoadingAllLines: 'false', initialLineDisplayLimit: 50});
     expect(res).to.be.an('array');
     expect(res.length).to.be(1);
-    expect(res[0].lines.length).to.be(gitParser.initialGitDiffLoadSize);
-    expect(res[0].unparsedLines).to.be(13);
+    expect(res[0].lines.length).to.be(50);
+    expect(res[0].totalNumberOfLines).to.be(63);
 
 
-    res = gitParser.parseGitDiff(sampleText, ['true']);
+    res = gitParser.parseGitDiff(sampleText, {isLoadingAllLines: 'true', initialLineDisplayLimit: 50});
     expect(res).to.be.an('array');
     expect(res.length).to.be(1);
     expect(res[0].lines.length).to.be(63);
-    expect(res[0].unparsedLines).to.be(0);
-  });
+    expect(res[0].totalNumberOfLines).to.be(63);
 
+    res = gitParser.parseGitDiff(sampleText, {isLoadingAllLines: 'false', initialLineDisplayLimit: 100});
+    expect(res).to.be.an('array');
+    expect(res.length).to.be(1);
+    expect(res[0].lines.length).to.be(63);
+    expect(res[0].totalNumberOfLines).to.be(63);
+
+    sampleText += '1\n2\n3\n4\n5\n6\n7\n8\n9\n0\n1\n2\n3\n4\n5\n6\n7\n8\n9\n0\n1\n2\n3\n4\n5\n6\n7\n8\n9\n0\n';
+    sampleText += '1\n2\n3\n4\n5\n6\n7\n8\n9\n0\n1\n2\n3\n4\n5\n6\n7\n8\n9\n0\n1\n2\n3\n4\n5\n6\n7\n8\n9\n0\n';
+    
+    res = gitParser.parseGitDiff(sampleText, {isLoadingAllLines: 'false', initialLineDisplayLimit: 100});
+    expect(res).to.be.an('array');
+    expect(res.length).to.be(1);
+    expect(res[0].lines.length).to.be(100);
+    expect(res[0].totalNumberOfLines).to.be(123);
+  });
 });
 
