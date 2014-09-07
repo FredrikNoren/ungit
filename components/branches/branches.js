7f26e4516bfedbff45838f8778608c7111eee0ce
anbuf <anthony@selectivefew.com>
2014-09-06 23:05:13 -0400

    Replace 'fetch' by 'checkout'

--- components/branches/branches.js
+++ components/branches/branches.js
@@ -20,18 +20,18 @@ function BranchesViewModel(server, repoPath) {
     programEvents.dispatch({ event: 'current-branch-changed', newBranch: value });
   });
 
-  this.fetchLabel = ko.computed(function() {
-    if (self.currentBranch()) return 'Fetch nodes from ' + self.currentBranch();
+  this.checkoutLabel = ko.computed(function() {
+    if (self.currentBranch()) return 'Checkout ' + self.currentBranch();
     else return 'No branches specified';
   })
 
-  this.fetchingProgressBar = components.create('progressBar', { predictionMemoryKey: 'fetching-' + this.repoPath, temporary: true });
+  this.checkoutingProgressBar = components.create('progressBar', { predictionMemoryKey: 'checkouting-' + this.repoPath, temporary: true });
   
-  this.fetchEnabled = ko.computed(function() {
+  this.checkoutEnabled = ko.computed(function() {
     return self.branches().length > 0;
   });
   
-  this.shouldAutoFetch = ungit.config.autoFetch;
+  this.shouldAutoCheckout = false;
   this.updateBranches(); 
 }
 
@@ -43,21 +43,20 @@ BranchesViewModel.prototype.onProgramEvent = function(event) {
     event.event == 'working-tree-changed')
     this.updateBranches();
 }
-BranchesViewModel.prototype.clickFetch = function() { this.fetch({ nodes: true, tags: true }); }
+BranchesViewModel.prototype.clickCheckout = function() { this.checkout({ nodes: true, tags: true }); }
 BranchesViewModel.prototype.onProgramEvent = function(event) {
-  if (event.event == 'request-credentials') this.fetchingProgressBar.pause();
-  else if (event.event == 'request-credentials-response') this.fetchingProgressBar.unpause();
-  else if (event.event == 'request-fetch-tags') this.fetch({ tags: true });
+  if (event.event == 'request-credentials') this.checkoutingProgressBar.pause();
+  else if (event.event == 'request-credentials-response') this.checkoutingProgressBar.unpause();
+  else if (event.event == 'request-checkout-tags') this.checkout({ tags: true });
 }
-BranchesViewModel.prototype.fetch = function(options) {
-  if (this.fetchingProgressBar.running()) return;
+BranchesViewModel.prototype.checkout = function(options) {
+  if (this.checkoutingProgressBar.running()) return;
   var self = this;
 
-  this.fetchingProgressBar.start();
+  this.checkoutingProgressBar.start();
   var jobs = [];
-  //if (options.nodes) jobs.push(function(done) { self.server.post('/fetch', { path: self.repoPath, branch: self.currentBranch() }, done);  });
   async.parallel(jobs, function(err, result) {
-    self.fetchingProgressBar.stop();
+    self.checkoutingProgressBar.stop();
   });
 }
 
@@ -79,11 +78,11 @@ BranchesViewModel.prototype.updateBranches = function() {
         self.currentBranch(branch.name);
       else
         self.currentBranch(branches[0].name);
-      if (self.shouldAutoFetch) {
-        self.fetch({ nodes: true, tags: true });
+      if (self.shouldAutoCheckout) {
+        self.checkout({ nodes: true, tags: true });
       }
     }
-    self.shouldAutoFetch = false;
+    self.shouldAutoCheckout = false;
   });
 }
 BranchesViewModel.prototype.showAddBranchDialog = function() {
