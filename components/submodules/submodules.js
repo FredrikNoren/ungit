26de6f34028f47fdc6a8295986b7efe35ec9d520
codingtwinky <twinky@codingtwinky.com>
2014-11-04 22:47:28 -0800

    Adding updateSubmodules

--- components/submodules/submodules.js
+++ components/submodules/submodules.js
@@ -15,6 +15,8 @@ function SubmodulesViewModel(server, repoPath) {
   this.server = server;
   
   this.submodules = ko.observableArray();
+  
+  this.updateProgressBar = components.create('progressBar', { predictionMemoryKey: 'Updating Submodules', temporary: true });
 }
 
 SubmodulesViewModel.prototype.updateNode = function(parentElement) {
@@ -31,5 +33,15 @@ SubmodulesViewModel.prototype.updateNode = function(parentElement) {
     if (self.submodules().length > 0) {
       ko.renderTemplate('submodules', self, {}, parentElement);
     }
+  });
+}
+
+SubmodulesViewModel.prototype.updateSubmodules = function(parentElement) {
+  if (this.updateProgressBar.running()) return;
+  var self = this;
+  
+  this.updateProgressBar.start();
+  this.server.post('/submodules/update', { path: this.repoPath }, function(err, result) {
+    self.updateProgressBar.stop();
   });
 }
