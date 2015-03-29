addeefaa257e3d8e4e3f3a3be5d5d939dbe094d0
codingtwinky <twinky@codingtwinky.com>
2015-03-29 14:19:37 -0700

    fix typo

--- clicktests/test.remotes.js
+++ clicktests/test.remotes.js
@@ -60,7 +60,7 @@ suite.test('Fetch from newly added remote', function(done) {
   });
 });
 
-suite.test('Submodule delete check', function(done) {
+suite.test('Remote delete check', function(done) {
   helpers.click(page, '[data-ta-clickable="remotes-menu"]');
   helpers.waitForElement(page, '[data-ta-container="remotes"]', function() {
     helpers.click(page, '[data-ta-clickable="myremote-remove"]');
