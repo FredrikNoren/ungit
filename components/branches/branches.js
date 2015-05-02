043e1519a70acc4c0d65e52f1f72e42ec0ee6213
codingtwinky <twinky@codingtwinky.com>
2015-05-01 19:29:20 -0700

    Added branches

---
+++ components/branches/branches.js
@@ -0,0 +1,36 @@
+
+var ko = require('knockout');
+var _ = require('lodash');
+var async = require('async');
+var components = require('ungit-components');
+var programEvents = require('ungit-program-events');
+
+components.register('branches', function(args) {
+  return new BranchesViewModel(args.server, args.repoPath);
+});
+
+function BranchesViewModel(server, repoPath) {
+  var self = this;
+  this.repoPath = repoPath;
+  this.server = server;
+  this.branches = ko.observableArray();
+  this.fetchingProgressBar = components.create('progressBar', { predictionMemoryKey: 'fetching-' + this.repoPath, temporary: true });
+  this.fetchEnabled = true;
+  this.fetchLabel = 'Branches';
+  this.updateBranches();
+}
+BranchesViewModel.prototype.updateNode = function(parentElement) {
+  ko.renderTemplate('branches', this, {}, parentElement);
+}
+BranchesViewModel.prototype.clickFetch = function() { this.updateBranches(); }
+BranchesViewModel.prototype.onProgramEvent = function(event) {
+  if (event.event === 'working-tree-changed' || event.event == 'request-app-content-refresh') {
+    this.updateBranches();
+  }
+}
+BranchesViewModel.prototype.checkoutBranch = function(branch) {
+  // checkout branch
+}
+BranchesViewModel.prototype.updateBranches = function() {
+  // update branch list
+}
