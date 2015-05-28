3b60b420c67bfda84a2055e399d6299402233c58
codingtwinky <twinky@codingtwinky.com>
2015-05-27 19:10:21 -0700

    Removed stupid console log

--- components/staging/staging.js
+++ components/staging/staging.js
@@ -98,7 +98,6 @@ StagingViewModel.prototype.setAheadAndBehind = function() {
       console.log("error while acquiring behind info", err);
       return;
     }
-    console.log(behind);
     self.behind(behind ? behind.split('\n') : []);
   });
 }
