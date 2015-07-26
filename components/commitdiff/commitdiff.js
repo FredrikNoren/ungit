68b62697139dc555f2b6a139ed3a360645b4ac67
codingtwinky <twinky@codingtwinky.com>
2015-07-25 21:38:28 -0700

    Removing unnecessary functions  from commit diff

--- components/commitdiff/commitdiff.js
+++ components/commitdiff/commitdiff.js
@@ -7,36 +7,14 @@ components.register('commitDiff', function(args) {
 });
 
 var CommitDiff = function(args) {
-  this.totalLineDiffs = ko.observable();
   this.commitLineDiffs = ko.observableArray();
-  this.showLoadMore = ko.observable();
-  this.server = args.server;
   this.sha1 = args.sha1;
-  this.repoPath = args.repoPath;
-  this.textDiffType = args.textDiffType;
-  this.maxNumberOfFilesShown = 50;
-
-  var totalLineDiffs = args.fileLineDiffs.shift();
-  if (!totalLineDiffs) {
-    this.totalLineDiffs([0, 0, 'total']);
-  } else {
-    this.totalLineDiffs(totalLineDiffs);
-  }
-
-  this.checkShowLoadMore(args.fileLineDiffs.length);
+  args.fileLineDiffs.shift();  // remove first line that has "total"
   this.loadFileLineDiffs(args);
 };
 
 CommitDiff.prototype.updateNode = function(parentElement) {
   ko.renderTemplate('commitdiff', this, {}, parentElement);
-};
-
-CommitDiff.prototype.checkShowLoadMore = function(size) {
-  if (size < this.maxNumberOfFilesShown) {
-    this.showLoadMore(false);
-  } else {
-    this.showLoadMore(true);
-  }
 };
 
 CommitDiff.prototype.loadFileLineDiffs = function(args) {
@@ -49,24 +27,4 @@ CommitDiff.prototype.loadFileLineDiffs = function(args) {
   });
 
   this.commitLineDiffs(this.commitLineDiffs().concat(tempCommitLineDiffs));
-}
-
-CommitDiff.prototype.loadMore = function(data, event) {
-  this.maxNumberOfFilesShown += 50;
-  var self = this;
-
-  this.server.get('/show', { path: this.repoPath, sha1: this.sha1 }, function(err, logEntries) {
-    if (err || !logEntries || !logEntries[0]) {
-      return;
-    }
-    self.checkShowLoadMore(logEntries[0].fileLineDiffs.length);
-    self.loadFileLineDiffs({
-      repoPath: self.repoPath,
-      server: self.server,
-      sha1: self.sha1,
-      textDiffType: self.textDiffType,
-      fileLineDiffs: logEntries[0].fileLineDiffs
-    });
-  });
-  event.stopImmediatePropagation();
 }
