5815381866822f482cc6de44fc1b219f312cd8fe
codingtwinky <twinky@codingtwinky.com>
2015-08-13 20:27:26 -0700

    adding few missing variables to nodes

--- components/graph/git-node.js
+++ components/graph/git-node.js
@@ -3,8 +3,10 @@ var components = require('ungit-components');
 var GitNodeViewModel = function(graph, logEntry) {
   this.graph = graph;
   this.logEntry = logEntry;
-  this.title = ko.observable(this.logEntry.message.split('\n')[0]);
   this.commitContainerVisible = ko.observable(true);
+  this.title = ko.observable(this.logEntry.message.split('\n')[0]);
+  this.parents = ko.observable(this.logEntry.parents || []);
+  this.commitTime = ko.observable(this.logEntry.commitDate);
   
   this.commitComponent = components.create('commit', {
     sha1: logEntry.sha1,
@@ -12,7 +14,6 @@ var GitNodeViewModel = function(graph, logEntry) {
     server: this.graph.server
   });
   this.commitComponent.setData(this.logEntry);
-  
 }
 module.exports = GitNodeViewModel;
 
