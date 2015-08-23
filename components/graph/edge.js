c78c3682d3d2be54b01fb38ed1ca8fc9473d78c5
codingtwinky <twinky@codingtwinky.com>
2015-08-23 15:36:51 -0700

    Make edgeVM computeable

--- components/graph/edge.js
+++ components/graph/edge.js
@@ -2,29 +2,25 @@ var ko = require('knockout');
 
 var EdgeViewModel = function(graph, nodeAsha1, nodeBsha1) {
   var self = this;
-  this.nodeA = graph.nodesById[nodeAsha1];
-  var pathPrefix = "M " + self.nodeA.cx() + " " + self.nodeA.cy();
-  this.nodeB = graph.nodesById[nodeBsha1];
-  this.path = pathPrefix + this.getPathToDest(this.nodeB);
+  this.nodeA = ko.observable(graph.nodesById[nodeAsha1]);
+  this.nodeB = ko.observable(graph.nodesById[nodeBsha1]);
+  this.path = ko.computed(function() {
+    var pathPrefix = "M " + self.nodeA().cx() + " " + self.nodeA().cy();
+    if (self.nodeB()) {
+      return pathPrefix + " L " + self.nodeB().cx() + " " + self.nodeB().cy();
+    } else {
+      return pathPrefix + " l 0 99999";
+    }
+  });
   
-  if (!this.nodeB) {
+  if (!this.nodeB()) {
     // if "to" node doesn't exist, watch for it and update and unsubscribe
     var nodeWatcher = graph.nodes.subscribe(function() {
-      self.nodeB = graph.nodesById[nodeBsha1];
+      self.nodeB(graph.nodesById[nodeBsha1]);
       
-      if (!self.nodeB) return;
-      
-      self.path = pathPrefix + self.getPathToDest(self.nodeB);
+      if (!self.nodeB()) return;
       nodeWatcher.dispose();
     });
   }
 }
 module.exports = EdgeViewModel;
-
-EdgeViewModel.prototype.getPathToDest = function(nodeB) {
-  if (nodeB) {
-    return " L " + this.nodeB.cx() + " " + this.nodeB.cy();
-  } else {
-    return " l 0 99999";
-  }
-}
