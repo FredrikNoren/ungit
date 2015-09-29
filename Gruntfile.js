c04d61643f904978a8e3948b79fdd8a03a5d2ba7
codingtwinky <twinky@codingtwinky.com>
2015-09-28 17:11:09 -0700

    adding snapsvg

--- Gruntfile.js
+++ Gruntfile.js
@@ -249,6 +249,7 @@ module.exports = function(grunt) {
     b.require('path', { expose: 'path' });
     b.require('diff2html', { expose: 'diff2html' });
     b.require('bluebird', { expose: 'bluebird' });
+    b.require('snapsvg', { expose: 'snapsvg' });
     var outFile = fs.createWriteStream('./public/js/ungit.js');
     outFile.on('close', function() {
       done();
