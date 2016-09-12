84d1b5ad32b583bb70942698f08990d9b4e483bb
codingtwinky <twinky@codingtwinky.com>
2016-09-11 18:15:40 -0700

    Fix diff options buttons to toggle for commitdiff

--- components/commitdiff/commitdiff.js
+++ components/commitdiff/commitdiff.js
@@ -2,18 +2,30 @@ var ko = require('knockout');
 var CommitLineDiff = require('./commitlinediff.js').CommitLineDiff;
 var components = require('ungit-components');
 
+var sideBySideDiff = 'sidebysidediff'
+var textDiff = 'textdiff'
+
 components.register('commitDiff', function(args) {
   return new CommitDiff(args);
 });
 
 var CommitDiff = function(args) {
+  var self = this;
   this.commitLineDiffs = ko.observableArray();
   this.sha1 = args.sha1;
 
   // parent components can provide their own buttons (e.g. staging component)
   this.showDiffButtons = ko.observable(!args.textDiffType);
+  this.textDiffTypeTitle = ko.observable("Default");
   this.textDiffType = args.textDiffType = args.textDiffType || ko.observable('textdiff');
+  this.textDiffType.subscribe(function(value) {
+    self.textDiffTypeTitle(value === textDiff ? "Default" : "Side By Side");
+  });
+  this.wordWrapTitle = ko.observable("No Word Wrap");
   this.wordWrap = args.wordWrap = args.wordWrap || ko.observable(false);
+  this.wordWrap.subscribe(function(value) {
+    self.wordWrapTitle(value ? "Word Wrap" : "No Word Wrap");
+  });
 
   args.fileLineDiffs.shift();  // remove first line that has "total"
   this.loadFileLineDiffs(args);
@@ -33,9 +45,9 @@ CommitDiff.prototype.loadFileLineDiffs = function(args) {
 
   this.commitLineDiffs(this.commitLineDiffs().concat(tempCommitLineDiffs));
 }
-CommitDiff.prototype.textDiffTypeChange = function(type) {
-  this.textDiffType(type);
+CommitDiff.prototype.textDiffTypeToggle = function(type) {
+  this.textDiffType(this.textDiffType() === textDiff ? sideBySideDiff : textDiff);
 }
-CommitDiff.prototype.wordWrapChange = function(state) {
-  this.wordWrap(state);
+CommitDiff.prototype.wordWrapToggle = function(state) {
+  this.wordWrap(!this.wordWrap());
 }
