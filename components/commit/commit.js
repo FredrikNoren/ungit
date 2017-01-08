c64bd9d50dc26e2c93ee8dc6b2976f3154cc69ff
Neil Kalman <Neilkalman@gmail.com>
2017-01-08 17:37:48 +0200

    also trim the email as suggested in https://en.gravatar.com/site/implement/hash/

--- components/commit/commit.js
+++ components/commit/commit.js
@@ -33,7 +33,7 @@ function CommitViewModel(gitNode) {
   this.numberOfRemovedLines = ko.observable();
   this.authorGravatar = ko.computed(function() {
     var lowerCaseEmail = self.authorEmail();
-    lowerCaseEmail = lowerCaseEmail ? lowerCaseEmail.toLowerCase() : lowerCaseEmail;
+    lowerCaseEmail = lowerCaseEmail ? lowerCaseEmail.toLowerCase().trim() : lowerCaseEmail;
     return md5(lowerCaseEmail);
   });
 
