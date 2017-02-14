be9b0fad30541a9b02be132db286462aa917cd6a
codingtwinky <twinky@codingtwinky.com>
2017-02-13 18:29:20 -0800

    Add in comments and timeout

--- source/utils/cache.js
+++ source/utils/cache.js
@@ -4,41 +4,69 @@ const cache = Bluebird.promisifyAll(new NodeCache({ stdTTL: 0, errorOnMissing: t
 const md5 = require('blueimp-md5');
 const funcMap = {}; // Will there ever be a use case where this is a cache with TTL? func registration with TTL?
 
+/**
+ * @function resolveFunc
+ * @description Get cached result associated with the key or execute a function to get the result
+ * @param {string} [key] - A key associated with a function to be executed.
+ * @return {Promise} - Promise either resolved with cached result of the function or rejected with function not found.
+ */
 cache.resolveFunc = (key) => {
   return cache.getAsync(key) // Cant do `cache.getAsync(key, true)` due to `get` argument ordering...
     .catch({ errorcode: "ENOTFOUND" }, (e) => {
       if (!funcMap[key]) throw e;     // func associated with key is not found, throw not found error
-      const result = funcMap[key]();  // func is found, resolve, set with TTL and return result
-      return cache.setAsync(key, result, cache.options.stdTTL)
+      const result = funcMap[key].func();  // func is found, resolve, set with TTL and return result
+      return cache.setAsync(key, result, funcMap[key].ttl)
         .then(() => { return result });
     });
 }
 
-cache.registerFunc = (key, func) => {
-  let checkedKey = key;
-  let checkedFunc = func;
+/**
+ * @function registerFunc
+ * @description Register a function to cache it's result. If same key exists, key is deregistered and registered again.
+ * @param {ttl} [ttl=0] - ttl to be used for the cached result of function.
+ * @param {string} [key=md5 of func] - Key to retrive cached function result.
+ * @param {function} [func] - Function to be executed to get the result.
+ * @return {string} - key to retrive cached function result.
+ */
+cache.registerFunc = (...args) => {
+  let func = args.pop();
+  let key = args.pop() || md5(func);
+  let ttl = args.pop() || cache.options.stdTTL;
 
-  if (typeof key === "function") {
-    checkedFunc = key
-    checkedKey = md5(checkedFunc);
+  if (typeof func !== "function") {
+    throw new Error("no function was passed in.");
   }
 
-  if (typeof checkedFunc !== "function") {
-    throw new Error("no function was passed in");
+  if (isNaN(ttl) || ttl < 0) {
+    throw new Error("ttl value is not valid.");
   }
 
-  if (funcMap[checkedKey]) {
-    cache.deregisterFunc(checkedKey);
+  if (funcMap[key]) {
+    cache.deregisterFunc(key);
   }
-  funcMap[checkedKey] = checkedFunc;
 
-  return checkedKey;
+  funcMap[key] = {
+    func: func,
+    ttl: ttl
+  }
+
+  return key;
 }
 
+/**
+ * @function invalidateFunc
+ * @description Immediately invalidate cached function result despite ttl value
+ * @param {string} [key] - A key associated with a function to be executed.
+ */
 cache.invalidateFunc = (key) => {
   cache.del(key);
 }
 
+/**
+ * @function deregisterFunc
+ * @description Remove function registration and invalidate it's cached value.
+ * @param {string} [key] - A key associated with a function to be executed.
+ */
 cache.deregisterFunc = (key) => {
   cache.invalidateFunc(key);
   delete funcMap[key];
