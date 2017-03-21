14db1b4d463ff2da26e09c4593ba599efb40ac12
codingtwinky <twinky@codingtwinky.com>
2017-03-20 17:05:26 -0700

    Promisify test execution

--- clicktests/testsuite.js
+++ clicktests/testsuite.js
@@ -15,21 +15,17 @@ testsuites.runAllSuits = function() {
   if (testsuites.isRunning) return;
   testsuites.isRunning = true;
   var startTime = Date.now();
-  var i = 0;
-  function runNext() {
-    if (i == testsuites.suites.length) {
-      console.log('Finnished all test suites. Took ' + (Date.now() - startTime) / 1000 + 'sec (' + testsuites.suites.length + ' suites)');
-      phantom.exit(0);
-    }
-    var suite = testsuites.suites[i];
-    i++;
-    helpers.log(cliColor.set('#### Running suite: ' + suite.name + ' (' + i + '/' + testsuites.suites.length + ')', 'blue'));
-    suite.suite.run(suite.name, function() {
-      helpers.log(cliColor.set('#### Done with suite: ' + suite.name + ' (' + i + '/' + testsuites.suites.length + ')', 'blue'));
-      setTimeout(runNext, 1000);
-    });
-  }
-  runNext();
+  return Bluebird.each(testsuites.suites, function(suite, i) {
+    var suiteIdentifier = suite.name + ' (' + i + '/' + testsuites.suites.length + ')';
+    helpers.log(cliColor.set('#### Running suite: ' + suiteIdentifier, 'blue'));
+    return suite.suite.run(suite.name)
+      .then(function() {
+        helpers.log(cliColor.set('#### Done with suite: ' + suiteIdentifier, 'blue'));
+      });
+  }).then(function() {
+    console.log('Finished all test suites. Took ' + (Date.now() - startTime) / 1000 + 'sec (' + testsuites.suites.length + ' suites)');
+    phantom.exit(0);
+  });
 }
 
 function TestSuite(page, config) {
@@ -41,7 +37,7 @@ function TestSuite(page, config) {
 TestSuite.prototype.test = function(name, description) {
   this.tests.push({ name: name, description: description });
 }
-TestSuite.prototype.run = function(suiteName, callback) {
+TestSuite.prototype.run = function(suiteName) {
   var self = this;
   var startTime = Date.now();
 
@@ -60,13 +56,14 @@ TestSuite.prototype.run = function(suiteName, callback) {
     }).timeout(self.config.timeout)
     .then(function() {
       console.log('All tests in suite ok! Took ' + (Date.now() - startTime) / 1000 + 'sec (' + self.tests.length + ' tests)');
-      callback();
     }).catch(function(err) {
       console.error('Tests failed! - ', err);
       phantom.exit(1);
     });
 }
 
+// Ungit had an amazing foresight to not use leftpad and avoid it's chaos...
+// https://www.theregister.co.uk/2016/03/23/npm_left_pad_chaos/
 function pad(num, size) {
     var s = num+"";
     while (s.length < size) s = "0" + s;
