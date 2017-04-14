412a1010a6bbb1ea797af4eb9f8f1a67f102e1d6
codingtwinky <twinky@codingtwinky.com>
2017-04-14 16:21:06 -0700

    Enabling logs

--- clicktests/environment.js
+++ clicktests/environment.js
@@ -14,7 +14,7 @@ function Environment(page, config) {
   this.config.rootPath = (typeof this.config.rootPath === 'string') ? this.config.rootPath : '';
   this.config.serverTimeout = this.config.serverTimeout || 15000;
   this.config.viewportSize = this.config.viewportSize || { width: 2000, height: 2000 };
-  this.config.showServerOutput = this.config.showServerOutput || false;
+  this.config.showServerOutput = this.config.showServerOutput || true;
   this.config.serverStartupOptions = this.config.serverStartupOptions || [];
   this.url = 'http://localhost:' + this.config.port + this.config.rootPath;
 }
@@ -114,6 +114,7 @@ Environment.prototype.startServer = function(callback) {
     '--maxNAutoRestartOnCrash=0',
     '--no-autoCheckoutOnBranchCreate',
     '--alwaysLoadActiveBranch',
+    '--logLevel=debug',
     '--logGitCommands']
     .concat(this.config.serverStartupOptions);
   var ungitServer = child_process.spawn('node', options);
