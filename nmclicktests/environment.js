b276d6ac8322bc2041a4036f69d853227c4b9211
codingtwinky <twinky@codingtwinky.com>
2017-04-23 11:16:15 -0700

    es6ify

--- nmclicktests/environment.js
+++ nmclicktests/environment.js
@@ -5,218 +5,213 @@ const Nightmare = require('nightmare');
 const net = require('net');
 let portrange = 45032;
 
-module.exports = (config) => { return new Environment(config); }
-
-// Environment provides
-function Environment(config) {
-  this.nightmare = Nightmare({ Promise: Bluebird });
-  this.config = config || {};
-  this.config.rootPath = (typeof this.config.rootPath === 'string') ? this.config.rootPath : '';
-  this.config.serverTimeout = this.config.serverTimeout || 15000;
-  this.config.viewWidth = 2000;
-  this.config.viewHeight = 2000;
-  this.config.showServerOutput = this.config.showServerOutput || true;
-  this.config.serverStartupOptions = this.config.serverStartupOptions || [];
-  this.shuttinDown = false;
-}
-
-Environment.prototype.getPort = function() {
-  var self = this;
-  portrange += 1;
-
-  return new Bluebird(function(resolve, reject) {
-    var server = net.createServer();
-
-    server.listen(portrange, function (err) {
-      server.once('close', function () {
-        self.port = portrange;
-        self.url = 'http://localhost:' + self.port + self.config.rootPath;
-        resolve();
-      });
-      server.close();
-    });
-    server.on('error', function (err) {
-      resolve(self.getPort());
-    });
-  });
-}
-
-Environment.prototype.log = function(text) {
-  console.log((new Date()).toISOString(), text);
-}
-
-Environment.prototype.init = function() {
-  var self = this;
-  this.setupPage();
-  return this.getPort()
-    .then(function() { return self.startServer(); })
-    .then(function() { return self.ensureStarted(); })
-    .timeout(7000)
-    .catch(function(err) { throw new Error("Cannot confirm ungit start!!"); })
-    .then(function() { return self.createTempFolder(); })
-    .then(function(res) { self.path = res.path });
-}
-Environment.prototype.shutdown = function(doNotClose) {
-  var self = this;
-  this.shuttinDown = true;
-  return this.backgroundAction('POST', this.url + '/api/testing/cleanup')
-    .then(function() { return self.shutdownServer(); })
-    .then(function() { if (!doNotClose) self.nightmare.end(); });
-}
-Environment.prototype.createCommits = function(config, limit, x) {
-  var self = this;
-  x = x || 0
-  if (!limit || limit < 0 || x === limit) return Bluebird.resolve();
-
-  return self.createTestFile(config.path + '/testy' + x)
-    .then(function() {
-      return self.backgroundAction('POST', self.url + '/api/commit', {
-        path: config.path,
-        message: 'Init Commit ' + x,
-        files: [{ name: 'testy' + x }]
-      });
-    }).then(function() { return self.createCommits(config, limit, x + 1); })
-}
-Environment.prototype.createRepos = function(config) {
-  var self = this;
-  return Bluebird.map(config, function(conf) {
-    return self.createFolder(conf.path)
-      .then(function() { return self.initFolder({ bare: !!conf.bare, path: conf.path }); })
-      .then(function() { return self.createCommits(conf, conf.initCommits); })
-  });
-}
-Environment.prototype.setupPage = function() {
-  var self = this;
-  this.nightmare.viewport(this.config.viewWidth, this.config.viewHeight);
-  this.nightmare.on('console', function(type, msg) {
-    self.log(`[ui] ${type} - ${msg}`);
-
-    if (type === 'error' && !self.shuttinDown) {
-      self.log('ERROR DETECTED!');
-      process.exit(1);
-    }
-  });
-}
-
-Environment.prototype.ensureStarted = function() {
-  var self = this;
-  return Bluebird.resolve()
-    .then(function() {
-      if (!self.hasStarted) {
-        return Bluebird.resolve()
-          .delay(50)
-          .then(function() { return self.ensureStarted(); });
-      }
-    });
-}
-
-Environment.prototype.goto = function(url) {
-  this.nightmare = this.nightmare.goto(url);
-  return this.nightmare;
-}
-
-Environment.prototype.startServer = function() {
-  var self = this;
-  this.log('Starting ungit server...', this.config.serverStartupOptions);
-
-  self.hasStarted = false;
-  var options = ['bin/ungit',
-    '--cliconfigonly',
-    '--port=' + self.port,
-    '--rootPath=' + self.config.rootPath,
-    '--no-launchBrowser',
-    '--dev',
-    '--no-bugtracking',
-    '--no-sendUsageStatistics',
-    '--autoShutdownTimeout=' + self.config.serverTimeout,
-    '--logLevel=debug',
-    '--maxNAutoRestartOnCrash=0',
-    '--no-autoCheckoutOnBranchCreate',
-    '--alwaysLoadActiveBranch',
-    '--logGitCommands']
-    .concat(self.config.serverStartupOptions);
-  var ungitServer = child_process.spawn('node', options);
-  ungitServer.stdout.on("data", function (data) {
-    if (self.config.showServerOutput) self.log(prependLines('[server] ', data.toString()));
-
-    if (data.toString().indexOf('Ungit server already running') >= 0) {
-      self.log('server-already-running');
-    }
-
-    if (data.toString().indexOf('## Ungit started ##') >= 0) {
-      if (self.hasStarted) {
-        self.log('Ungit started twice, probably crashed.');
-      } else {
-        self.hasStarted = true;
-        self.log('Ungit server started.');
-      }
-    }
-  });
-  ungitServer.stderr.on("data", function (data) {
-    self.log(prependLines('[server ERROR] ', data.toString()));
-    if (data.indexOf("EADDRINUSE") > -1) {
-      self.log("retrying with different port");
-      ungitServer.kill('SIGINT');
-      self.getPort().then(function() { return self.startServer(); });
-    }
-  });
-  ungitServer.on('exit', function() {
-    self.log('UNGIT SERVER EXITED');
-  });
-  return Bluebird.resolve();
-}
-
-
-
-Environment.prototype.backgroundAction = function(method, url, body) {
-  if (method === 'GET') {
-    url += getUrlArgument(body);
-    body = null;
-  } else {
-    body = JSON.stringify(body);
-  }
-
-  return this.nightmare.goto(url, getRestSetting(method), body)
-    .evaluate(function() { return document.querySelector('pre').innerHTML; })
-    .then(function(data) {
-      try { data = JSON.parse(data); } catch(ex) {}
-      return data;
-    });
-}
-Environment.prototype.createTestFile = function(filename) {
-  return this.backgroundAction('POST', this.url + '/api/testing/createfile', { file: filename });
-}
-Environment.prototype.changeTestFile = function(filename) {
-  return this.backgroundAction('POST', this.url + '/api/testing/changefile', { file: filename });
-}
-Environment.prototype.shutdownServer = function() {
-  return this.backgroundAction('POST', this.url + '/api/testing/shutdown');
-}
-Environment.prototype.createTempFolder = function() {
-  this.log('Creating temp folder');
-  return this.backgroundAction('POST', this.url + '/api/testing/createtempdir');
-}
-Environment.prototype.createFolder = function(dir) {
-  this.log('Create folder: ' + dir);
-  return this.backgroundAction('POST', this.url + '/api/createdir', { dir: dir });
-}
-Environment.prototype.initFolder = function(options) {
-  return this.backgroundAction('POST', this.url + '/api/init', options);
-}
-Environment.prototype.gitCommand = function(options) {
-  return this.backgroundAction('POST', this.url + '/api/testing/git', options);
-}
+module.exports = (config) => new Environment(config);
 
 const getRestSetting = (method) => {
   return { method: method, encoding: 'utf8', 'cache-control': 'no-cache', 'Content-Type': 'application/json'};
 }
 
 const getUrlArgument = (data) => {
-  return Object.keys(data).map(function(k) {
-      return encodeURIComponent(k) + '=' + encodeURIComponent(data[k])
+  return Object.keys(data).map((k) => {
+      return `${encodeURIComponent(k)}=${encodeURIComponent(data[k])}`
     }).join('&')
 }
 
 const prependLines = (pre, text) => {
-  return text.split('\n').filter(function(l) { return l; }).map(function(line) { return pre + line; }).join('\n');
+  return text.split('\n').filter((l) => l)
+    .map((line) => pre + line)
+    .join('\n');
+}
+
+// Environment provides
+class Environment {
+  constructor(config) {
+    this.nightmare = Nightmare({ Promise: Bluebird });
+    this.config = config || {};
+    this.config.rootPath = (typeof this.config.rootPath === 'string') ? this.config.rootPath : '';
+    this.config.serverTimeout = this.config.serverTimeout || 15000;
+    this.config.viewWidth = 2000;
+    this.config.viewHeight = 2000;
+    this.config.showServerOutput = this.config.showServerOutput || true;
+    this.config.serverStartupOptions = this.config.serverStartupOptions || [];
+    this.shuttinDown = false;
+
+    // init
+    this.nightmare.viewport(this.config.viewWidth, this.config.viewHeight);
+    this.nightmare.on('console', (type, msg) => {
+      this.log(`[ui] ${type} - ${msg}`);
+
+      if (type === 'error' && !this.shuttinDown) {
+        this.log('ERROR DETECTED!');
+        process.exit(1);
+      }
+    })
+  }
+
+  getPort() {
+    portrange += 1;
+
+    return new Bluebird((resolve, reject) => {
+      const server = net.createServer();
+
+      server.listen(portrange, (err) => {
+        server.once('close', () => {
+          this.port = portrange;
+          this.url = `http://localhost:${this.port}${this.config.rootPath}`
+          resolve();
+        });
+        server.close();
+      });
+      server.on('error', (err) => {
+        this.getPort().then(resolve);
+      });
+    });
+  }
+
+  log(text) {
+    console.log((new Date()).toISOString(), text);
+  }
+
+  ensureStarted() {
+    return Bluebird.resolve()
+      .then(() => {
+        if (!this.hasStarted) {
+          return Bluebird.resolve()
+            .delay(50)
+            .then(() => { return this.ensureStarted(); });
+        }
+      });
+  }
+
+  init() {
+    return this.getPort()
+      .then(() => this.startServer())
+      .then(() => this.ensureStarted())
+      .timeout(7000)
+      .catch((err) => { throw new Error("Cannot confirm ungit start!!"); })
+      .then(() => this.createTempFolder())
+      .then((res) => this.path = res.path);
+  }
+
+  createRepos(config) {
+    return Bluebird.map(config, (conf) => {
+      return this.createFolder(conf.path)
+        .then(() => this.initFolder({ bare: !!conf.bare, path: conf.path }))
+        .then(() => this.createCommits(conf, conf.initCommits));
+    });
+  }
+
+  shutdown(doNotClose) {
+    this.shuttinDown = true;
+    return this.backgroundAction('POST', `${this.url}/api/testing/cleanup`)
+      .then(() => this.shutdownServer())
+      .then(() => { if (!doNotClose) this.nightmare.end(); });
+  }
+
+  createCommits(config, limit, x) {
+    x = x || 0
+    if (!limit || limit < 0 || x === limit) return Bluebird.resolve();
+
+    return this.createTestFile(`${config.path}/testy${x}`)
+      .then(() => {
+        return this.backgroundAction('POST', `${this.url}/api/commit`, {
+          path: config.path,
+          message: `Init Commit ${x}`,
+          files: [{ name: `testy${x}` }]
+        });
+      }).then(() => this.createCommits(config, limit, x + 1))
+  }
+
+  goto(url) {
+    this.nightmare = this.nightmare.goto(url);
+    return this.nightmare;
+  }
+
+  startServer() {
+    this.log('Starting ungit server...', this.config.serverStartupOptions);
+
+    this.hasStarted = false;
+    const options = ['bin/ungit',
+      '--cliconfigonly',
+      `--port=${this.port}`,
+      `--rootPath=${this.config.rootPath}`,
+      '--no-launchBrowser',
+      '--dev',
+      '--no-bugtracking',
+      '--no-sendUsageStatistics',
+      `--autoShutdownTimeout=${this.config.serverTimeout}`,
+      '--logLevel=debug',
+      '--maxNAutoRestartOnCrash=0',
+      '--no-autoCheckoutOnBranchCreate',
+      '--alwaysLoadActiveBranch',
+      '--logGitCommands']
+      .concat(this.config.serverStartupOptions);
+    const ungitServer = child_process.spawn('node', options);
+    ungitServer.stdout.on('data', (data) => {
+      if (this.config.showServerOutput) this.log(prependLines('[server] ', data.toString()));
+
+      if (data.toString().indexOf('Ungit server already running') >= 0) {
+        this.log('server-already-running');
+      }
+
+      if (data.toString().indexOf('## Ungit started ##') >= 0) {
+        if (this.hasStarted) {
+          this.log('Ungit started twice, probably crashed.');
+        } else {
+          this.hasStarted = true;
+          this.log('Ungit server started.');
+        }
+      }
+    });
+    ungitServer.stderr.on("data", (data) => {
+      this.log(prependLines('[server ERROR] ', data.toString()));
+      if (data.indexOf("EADDRINUSE") > -1) {
+        this.log("retrying with different port");
+        ungitServer.kill('SIGINT');
+        this.getPort().then(() => this.startServer());
+      }
+    });
+    ungitServer.on('exit', () => this.log('UNGIT SERVER EXITED'));
+    return Bluebird.resolve();
+  }
+
+  backgroundAction(method, url, body) {
+    if (method === 'GET') {
+      url += getUrlArgument(body);
+      body = null;
+    } else {
+      body = JSON.stringify(body);
+    }
+
+    return this.nightmare.goto(url, getRestSetting(method), body)
+      .evaluate(() => document.querySelector('pre').innerHTML)
+      .then((data) => {
+        try { data = JSON.parse(data); } catch(ex) {}
+        return data;
+      });
+  }
+
+  createTestFile(filename) {
+    return this.backgroundAction('POST', `${this.url}/api/testing/createfile`, { file: filename });
+  }
+  changeTestFile(filename) {
+    return this.backgroundAction('POST', `${this.url}/api/testing/changefile`, { file: filename });
+  }
+  shutdownServer() {
+    return this.backgroundAction('POST', `${this.url}/api/testing/shutdown`);
+  }
+  createTempFolder() {
+    this.log('Creating temp folder');
+    return this.backgroundAction('POST', `${this.url}/api/testing/createtempdir`);
+  }
+  createFolder(dir) {
+    this.log(`Create folder: ${dir}`);
+    return this.backgroundAction('POST', `${this.url}/api/createdir`, { dir: dir });
+  }
+  initFolder(options) {
+    return this.backgroundAction('POST', `${this.url}/api/init`, options);
+  }
+  gitCommand(options) {
+    return this.backgroundAction('POST', `${this.url}/api/testing/git`, options);
+  }
 }
