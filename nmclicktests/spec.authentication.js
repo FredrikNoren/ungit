df57428382fc74ecb2da53c5d253f5c55dceb1d6
codingtwinky <twinky@codingtwinky.com>
2017-04-22 18:21:50 -0700

    Complete authentication.js est

--- nmclicktests/spec.authentication.js
+++ nmclicktests/spec.authentication.js
@@ -1,20 +1,39 @@
 var expect = require('expect.js');
+var testuser = { username: 'testuser', password: 'testpassword' }
 var environment = require('./environment')({
-  serverStartupOptions: ['--authentication', '--users.testuser=testpassword'],
+  serverStartupOptions: ['--authentication', '--users.' + testuser.username + '=' + testuser.password],
   showServerOutput: true
 });
 
 describe('test authentication', function () {
-  it('Environment init', function() {
+  before('Environment init', function() {
     return environment.init();
   });
 
   it('Open home screen should show authentication dialog', function() {
-    return environment.nightmare.goto(environment.url)
+    return environment.goto(environment.url)
       .wait('[data-ta-container="login-page"]');
   });
 
-  it('Environment stop', function() {
+  it('Filling out the authentication with wrong details should result in an error', function() {
+    return environment.nightmare
+      .insert('[data-ta-container="login-page"] [data-ta-input="username"]', testuser.username)
+      .insert('[data-ta-container="login-page"] [data-ta-input="password"]', 'notthepassword')
+      .click('[data-ta-container="login-page"] [data-ta-clickable="submit"]')
+      .wait('[data-ta-element="login-error"]');
+  });
+
+  it('Filling out the authentication should bring you to the home screen', function() {
+    return environment.nightmare
+      .insert('[data-ta-container="login-page"] [data-ta-input="username"]')
+      .insert('[data-ta-container="login-page"] [data-ta-input="username"]', testuser.username)
+      .insert('[data-ta-container="login-page"] [data-ta-input="password"]')
+      .insert('[data-ta-container="login-page"] [data-ta-input="password"]', testuser.password)
+      .click('[data-ta-container="login-page"] [data-ta-clickable="submit"]')
+      .wait('[data-ta-container="home-page"]');
+  });
+
+  after('Environment stop', function() {
     return environment.shutdown();
   });
 });
