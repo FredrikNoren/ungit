8c77bf04a829ae354f65a585b75853c1263973e7
bes.internal <bes.internal@gmail.com>
2017-04-25 18:51:24 +0300

    Fix "effects of copying" typo error

--- components/crash/crash.js
+++ components/crash/crash.js
@@ -4,15 +4,14 @@ var components = require('ungit-components');
 var adBlocker = require('just-detect-adblock');
 
 components.register('crash', function(err, err2) {
-  return new LoginViewModel(err, err2);
+  return new CrashViewModel(err, err2);
 });
 
-var LoginViewModel = function(err, err2) {
-  var self = this;
+var CrashViewModel = function(err, err2) {
   if (adBlocker.isDetected()) {err='adblock'}
   this.eventcause = err || err2 || 'unknown error';
 }
 
-LoginViewModel.prototype.updateNode = function(parentElement) {
+CrashViewModel.prototype.updateNode = function(parentElement) {
   ko.renderTemplate('crash', this, {}, parentElement);
 }
