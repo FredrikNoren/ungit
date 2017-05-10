808a0bddf221c0f2fd4e579a0d6abe88f605ea14
bes.internal <bes.internal@gmail.com>
2017-05-10 17:20:16 +0300

    home screen repo list start from config's repo list

--- components/app/app.js
+++ components/app/app.js
@@ -16,7 +16,9 @@ var AppViewModel = function(appContainer, server) {
     this.header = components.create('header', { app: this });
   this.dialog = ko.observable(null);
 
-  this.repoList = ko.observableArray(JSON.parse(localStorage.getItem('repositories') || localStorage.getItem('visitedRepositories') || '[]')); // visitedRepositories is legacy, remove in the next version
+  var repoList=JSON.parse(localStorage.getItem('repositories') || '[]');
+  Array.prototype.unshift.apply(repoList, ungit.config.repos);
+  this.repoList = ko.observableArray(repoList);
   this.repoList.subscribe(function(newValue) { localStorage.setItem('repositories', JSON.stringify(newValue)); });
 
   this.content = ko.observable(components.create('home', { app: this }));
