da42b0579efd84064db2e4a88c259519a2f618fe
Yoann Lecuyer <yoann.lecuyer@gmail.com>
2018-01-31 21:21:18 +0100

    Add cancel button to staging view

--- nmclicktests/spec.generic.js
+++ nmclicktests/spec.generic.js
@@ -37,6 +37,12 @@ describe('[GENERIC]', () => {
       .wait('.commit');
   });
 
+  it('Should be possible to cancel amend a file', () => {
+    return environment.nm.ug.createTestFile(`${testRepoPaths[0]}/testfile2.txt`)
+      .ug.amendCommit()
+      .wait('.btn-stg-cancel');
+  });
+
   it('Should be able to add a new file to .gitignore', () => {
     return environment.nm.ug.createTestFile(`${testRepoPaths[0]}/addMeToIgnore.txt`)
       .wait('.files .file .btn-default')
@@ -164,6 +170,12 @@ describe('[GENERIC]', () => {
       .ug.moveRef('movebranch', 'Init');
   });
 
+  it('Should be possible to cancel creation of an empty commit', () => {
+    return environment.nm.ug.emptyCommit()
+      .ug.click('.btn-stg-cancel')
+      .wait('.empty-commit-link');
+  });
+
   it('Should be possible to create an empty commit', () => {
     return environment.nm.ug.emptyCommit()
       .wait('.commit');
@@ -174,6 +186,12 @@ describe('[GENERIC]', () => {
       .wait('.commit')
       .ug.amendCommit()
       .wait('.commit');
+  });
+
+  it('Should be possible to cancel amend of an empty commit', () => {
+    return environment.nm.ug.amendCommit()
+      .ug.click('.btn-stg-cancel')
+      .wait('.empty-commit-link');
   });
 
   it('Should be possible to click refresh button', () => {
