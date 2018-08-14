b21bb411d48cb7e40d7979a20d8860f53681efb6
Jan Pilzer <jan.pilzer@gmx.de>
2018-08-14 13:13:43 -0700

    Bind this correctly

--- public/source/storage.js
+++ public/source/storage.js
@@ -2,16 +2,20 @@
  * A wrapper around LocalStorage to support environments where LocalStorage is not available.
  * Stores and retrieves items from LocalStorage if available and uses a non-persistent cache otherwise.
  */
-var storage = {};
+var storage;
 try {
-    storage.getItem = localStorage.getItem;
-    storage.setItem = localStorage.setItem;
+    storage = {
+        getItem: localStorage.getItem.bind(localStorage),
+        setItem: localStorage.setItem.bind(localStorage),
+    };
 } catch (e) { /* Ignore Exception, use fallback implementation. */ }
 
 if (!storage) {
     var cache = Object.create(null);
-    storage.getItem = function (key) { return cache[key] || null; };
-    storage.setItem = function (key, value) { cache[key] = value; };
+    storage = {
+        getItem: function (key) { return cache[key] || null; },
+        setItem: function (key, value) { cache[key] = value; },
+    };
 }
 
-module.export = storage;
+module.exports = storage;
