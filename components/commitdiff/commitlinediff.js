2a7ad14ac2b7c23903857b8b018281d1d718fc1c
JK Kim <jung.kim.3627@gmail.com>
2018-09-09 17:36:35 -0700

    es6ifying some of the files

--- components/commitdiff/commitlinediff.js
+++ components/commitdiff/commitlinediff.js
@@ -1,38 +1,41 @@
-var ko = require('knockout');
-var components = require('ungit-components');
-var inherits = require('util').inherits;
-var programEvents = require('ungit-program-events');
+const ko = require('knockout');
+const components = require('ungit-components');
+const inherits = require('util').inherits;
+const programEvents = require('ungit-program-events');
 
-var CommitLineDiff = function(args, fileLineDiff) {
-  this.added = ko.observable(fileLineDiff[0]);
-  this.removed = ko.observable(fileLineDiff[1]);
-  this.fileName = ko.observable(fileLineDiff[2]);
-  this.fileType = fileLineDiff[3];
-  this.isShowingDiffs = ko.observable(false);
-  this.repoPath = args.repoPath;
-  this.server = args.server;
-  this.sha1 = args.sha1;
-  this.textDiffType = args.textDiffType;
-  this.wordWrap = args.wordWrap;
-  this.whiteSpace = args.whiteSpace;
-  this.specificDiff = ko.observable(this.getSpecificDiff());
-};
-exports.CommitLineDiff = CommitLineDiff;
+class CommitLineDiff {
+  constructor(args, fileLineDiff) {
+    this.added = ko.observable(fileLineDiff[0]);
+    this.removed = ko.observable(fileLineDiff[1]);
+    this.fileName = ko.observable(fileLineDiff[2]);
+    this.fileType = fileLineDiff[3];
+    this.isShowingDiffs = ko.observable(false);
+    this.repoPath = args.repoPath;
+    this.server = args.server;
+    this.sha1 = args.sha1;
+    this.textDiffType = args.textDiffType;
+    this.wordWrap = args.wordWrap;
+    this.whiteSpace = args.whiteSpace;
+    this.specificDiff = ko.observable(this.getSpecificDiff());
+  }
 
-CommitLineDiff.prototype.getSpecificDiff = function() {
-  return components.create(this.fileType + 'diff', {
-    filename: this.fileName(),
-    repoPath: this.repoPath,
-    server: this.server,
-    sha1: this.sha1,
-    textDiffType: this.textDiffType,
-    isShowingDiffs: this.isShowingDiffs,
-    whiteSpace: this.whiteSpace,
-    wordWrap: this.wordWrap
-  });
+  getSpecificDiff() {
+    return components.create(`${this.fileType}diff`, {
+      filename: this.fileName(),
+      repoPath: this.repoPath,
+      server: this.server,
+      sha1: this.sha1,
+      textDiffType: this.textDiffType,
+      isShowingDiffs: this.isShowingDiffs,
+      whiteSpace: this.whiteSpace,
+      wordWrap: this.wordWrap
+    });
+  }
+
+  fileNameClick() {
+    this.isShowingDiffs(!this.isShowingDiffs());
+    programEvents.dispatch({ event: 'graph-render' });
+  }
 }
 
-CommitLineDiff.prototype.fileNameClick = function() {
-  this.isShowingDiffs(!this.isShowingDiffs());
-  programEvents.dispatch({ event: 'graph-render' });
-};
+exports.CommitLineDiff = CommitLineDiff;
