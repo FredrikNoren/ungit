a148162ba1e6c6947d044e938afa1a2c8c4de95d
JK Kim <jung.kim.3627@gmail.com>
2020-02-08 09:08:54 -0800

    Fix graph height issue and making it more snappier

--- components/graph/graph.js
+++ components/graph/graph.js
@@ -56,10 +56,12 @@ class GraphViewModel {
       }
     });
     this.loadNodesFromApiThrottled = _.throttle(this.loadNodesFromApi.bind(this), 1000, {
-      leading: false,
+      leading: true,
+      trailing: false,
     });
     this.updateBranchesThrottled = _.throttle(this.updateBranches.bind(this), 1000, {
-      leading: false,
+      leading: true,
+      trailing: false,
     });
     this.graphWidth = ko.observable();
     this.graphHeight = ko.observable(800);
@@ -127,19 +129,20 @@ class GraphViewModel {
           prevNode = node;
         });
 
-        return this.computeNode(allNodes);
-      })
-      .then((nodes) => {
-        // create edges
+        const nodes = this.computeNode(allNodes);
+        let maxHeight = 0;
+
+        // create edges and calculate max height
         nodes.forEach((node) => {
+          if (node.cy() > maxHeight) {
+            maxHeight = node.cy();
+          }
           node.parents().forEach((parentSha1) => {
             this.getEdge(node.sha1, parentSha1);
           });
         });
 
-        if (nodes.length > 0) {
-          this.graphHeight(nodes[nodes.length - 1].cy() + 80);
-        }
+        this.graphHeight(maxHeight + 80);
         this.graphWidth(1000 + this.highestBranchOrder * 90);
         programEvents.dispatch({ event: 'init-tooltip' });
 
