a71536c69ec072db68f1b52df69ead64ce46df29
Jan Pilzer <jan.pilzer@gmx.de>
2020-04-27 01:01:32 -0700

    Style autocompletes like dropdowns (#1327)
    * Style autocomplete like dropdowns
    
    * Align autocomplete ref icon placement
    
    * Fixed width for dropdown/autocomplete icons

--- components/graph/git-node.js
+++ components/graph/git-node.js
@@ -165,8 +165,11 @@ class GitNodeViewModel extends Animateable {
     const $textBox = $(textBox);
 
     if (!$textBox.autocomplete('instance')) {
-      const renderItem = (ul, item) => $(`<li><a>${item.dom}</a></li>`).appendTo(ul);
+      const renderItem = (ul, item) => $(`<li><a>${item.displayHtml()}</a></li>`).appendTo(ul);
       $textBox.autocomplete({
+        classes: {
+          'ui-autocomplete': 'dropdown-menu'
+        },
         source: this.refs().filter(ref => !ref.isHEAD),
         minLength: 0,
         create: (event) => {
