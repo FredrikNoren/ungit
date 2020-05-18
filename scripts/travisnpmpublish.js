41af7230b17337d8e26194a2b671d3e241897ca2
campersau <buchholz.bastian@googlemail.com>
2020-05-18 10:09:24 +0200

    add some more logging

--- scripts/travisnpmpublish.js
+++ scripts/travisnpmpublish.js
@@ -7,6 +7,7 @@ if (
 ) {
   console.log('Skipping travis npm publish');
 } else {
+  console.log('Preparing travis npm publish');
   child_process.exec('git rev-parse --short HEAD', (err, stdout, stderr) => {
     const hash = stdout.trim();
     const packageJson = JSON.parse(fs.readFileSync('package.json'));
@@ -14,15 +15,18 @@ if (
     packageJson.version += `+${hash}`;
     fs.writeFileSync('package.json', `${JSON.stringify(packageJson, null, 2)}\n`);
     fs.writeFileSync('.npmrc', '//registry.npmjs.org/:_authToken=' + process.env.NPM_TOKEN);
+    console.log(`Publish ${packageJson.version} to npm`);
     child_process.exec('npm publish', (err) => {
       if (err) throw err;
-      else
+      else {
+        console.log(`Create and push tag v${version}`);
         child_process.exec(
           `git tag v${version} && git push -q https://${process.env.GITHUB_TOKEN}@github.com/FredrikNoren/ungit.git v${version}`,
           (err) => {
             if (err) throw err;
           }
         );
+      }
     });
   });
 }
