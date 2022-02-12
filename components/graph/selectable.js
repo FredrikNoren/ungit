526b27b7ebb0d562a1c822ca710b4d967d97852b
Jung (JK) Kim <jung.kim.3627@gmail.com>
2022-02-11 22:36:27 -0800

    (╯°□°)╯︵ ┻━┻

--- components/graph/selectable.js
+++ components/graph/selectable.js
@@ -7,6 +7,10 @@ class Selectable {
         return graph.currentActionContext() == this;
       },
       write(val) {
+        const valConstructorName = ((val || {}).constructor || {}).name;
+        const thisConstructorName = ((val || {}).constructor || {}).name;
+        ungit.logger.debug('>>>>581', typeof val, val === this);
+        ungit.logger.debug('>>>>888', valConstructorName, thisConstructorName);
         // val is this if we're called from a click ko binding
         if (val === this || val === true) {
           graph.currentActionContext(this);
