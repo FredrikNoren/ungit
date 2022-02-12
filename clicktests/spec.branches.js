a8d03bd8ba4ebfe63bace9dcdab6c905da5a3d49
Jung (JK) Kim <jung.kim.3627@gmail.com>
2022-02-11 21:21:07 -0800

    more debug

--- clicktests/spec.branches.js
+++ clicktests/spec.branches.js
@@ -205,6 +205,15 @@ describe('[BRANCHES]', () => {
   });
 
   it('test backward squash from own lineage', async () => {
+    environment.page.waitForFunction(() => {
+      const doms = document.querySelectorAll('.ref.branch')
+      doms.forEach(d => console.log('>>>>4123', d.getAttribute('data-ta-name'), d.classList.toString()))
+
+      const graph = ungit.__app.content().repository().graph
+      const target = graph.getRef('refs/heads/branch-1', false)
+      console.log('>>>>881', target.isHead, target.current(), target.isLocalBranch)
+      console.log('>>>>4123', graph.checkedOutBranch())
+    }, { polling: 500 })
     await environment.waitForBranch('branch-1');
     await environment.clickOnNode('.ref.branch.current');
     await environment.click('[data-ta-node-title="commit-1"] .squash .dropmask');
