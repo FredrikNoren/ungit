d659125de6e520ffc5b060932d177e3360cb34dd
campersau <buchholz.bastian@googlemail.com>
2022-09-08 12:04:53 +0200

    fix cancelAnimationFrame polyfill

--- public/source/main.js
+++ public/source/main.js
@@ -37,7 +37,9 @@ var { encodePath } = require('ungit-address-parser');
   var vendors = ['ms', 'moz', 'webkit', 'o'];
   for (var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
     window.requestAnimationFrame = window[vendors[x] + 'RequestAnimationFrame'];
-    window.cancelRequestAnimationFrame = window[vendors[x] + 'CancelRequestAnimationFrame'];
+    window.cancelAnimationFrame =
+      window[vendors[x] + 'CancelAnimationFrame'] ||
+      window[vendors[x] + 'CancelRequestAnimationFrame'];
   }
 
   if (!window.requestAnimationFrame)
