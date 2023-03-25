e8e317bcc4a96c08ee75df53fe67972bdffc4805
campersau <buchholz.bastian@googlemail.com>
2023-03-25 12:53:23 +0100

    2023

--- scripts/electronpackage.js
+++ scripts/electronpackage.js
@@ -11,7 +11,7 @@ electronPackager({
   all: process.argv.includes('--all'),
   asar: true,
   overwrite: true,
-  appCopyright: 'Copyright (c) 2013-2022 Fredrik Norén',
+  appCopyright: 'Copyright (c) 2013-2023 Fredrik Norén',
   ignore: [
     /^\/(?:[^/]+?\/)*(?:\..+|.+\.less)$/, // dot-files and less files anywhere
     /^\/(?:\..+|assets|clicktests|coverage|dist|scripts|test)\//, // folders in root
